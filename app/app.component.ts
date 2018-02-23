d057949edae2c42306edf8d7ee2eb449d54b0200
Danyel Murray <danyelmurray1987@gmail.com>
2018-02-23 10:14:04 -0800

    basic structure in place to add and edit animals.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
-
+import { Animal } from './animal.model';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1>My First Angular 2 App</h1>
-  `
+  <div class="container">
+     <h1>Animal Admissions: {{month}}/{{day}}/{{year}}</h1>
+     <h3>{{currentFocus}}</h3>
+     <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
+     <hr>
+     <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickedSender)="finishedEditing()"></edit-animal>
+     <new-animal (newAnimalSender)="addAnimal($event)"></new-animal>
+   </div>
+ `
 })
 
 export class AppComponent {
+ currentFocus: string = 'Animal Admissions';
+ currentTime = new Date();
+ month: number = this.currentTime.getMonth() + 1;
+ day: number = this.currentTime.getDate();
+ year: number = this.currentTime.getFullYear();
+ selectedAnimal: Animal = null;
 
+ masterAnimalList: Animal[] = [
+ ];
+
+ editAnimal(clickedAnimal) {
+   this.selectedAnimal = clickedAnimal;
+ };
+
+ finishedEditing() {
+   this.selectedAnimal = null;
+ }
+
+ addAnimal(newAnimalFromChild: Animal) {
+   this.masterAnimalList.push(newAnimalFromChild);
+ }
 }
